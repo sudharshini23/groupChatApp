1800845a1686409d6eca2a157d537c340245dbbc
sudharshini23 <sudarsh25@gmail.com>
2023-08-03 11:26:02 +0530

    Frontend SIgnup Page

---
+++ backend/public/signup.js
@@ -0,0 +1,42 @@
+const myForm = document.getElementById('my-form');
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const numberInput  = document.getElementById('number');
+const passwordInput = document.getElementById('password');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const number = numberInput.value;
+    const password = passwordInput.value;
+
+    const inputData = {
+        name,
+        email,
+        number,
+        password,
+    };
+
+    console.log(inputData);
+
+    try {
+        const response = axios.post("http://localhost:3000/user/signup", inputData)
+        console.log(response);
+        if(response.request.status === 201) {
+            alert(response.data.message);
+            window.location.href="./login.html";
+        }
+    }
+    catch(err) {
+        console.log(err);
+        console.log(err.response.data.error);
+        document.body.innerHTML+=`<div style="color:red;">${err.response.data.error}<div>`; 
+    }
+    nameInput.value = '';
+    emailInput.value='';
+    passwordInput.value = '';  
+}
